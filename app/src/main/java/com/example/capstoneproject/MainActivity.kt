7b4cdeb237d15a94ce82488ebbe7cc0a1b48b962
PjAsta05 <pujaastawa45@gmail.com>
2024-05-22 16:02:32 +0800

    Bottom Navbar + Fragment

---
+++ app/src/main/java/com/example/capstoneproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.capstoneproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.capstoneproject.databinding.ActivityMainBinding
+import com.example.capstoneproject.home.HomeFragment
+import com.example.capstoneproject.workshop.WorkshopFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigation.background = null
+
+        loadFragment(HomeFragment())
+        changeFragment()
+    }
+
+    private fun changeFragment() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.workshop -> {
+                    loadFragment(WorkshopFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
+    }
+}

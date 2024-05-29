8a6cfda2ab48fbf376b07e79324d84665ff76ed7
Pinda12 <vynnzo64@gmail.com>
2024-05-30 04:08:51 +0800

    ini form

--- app/src/main/java/com/example/capstoneproject/ui/process/ProcessAddActivity.kt
+++ app/src/main/java/com/example/capstoneproject/ui/process/ProcessAddActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.capstoneproject.R
 
-class ProcessActivity : AppCompatActivity() {
+class ProcessAddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_process)
+        setContentView(R.layout.activity_processadd)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

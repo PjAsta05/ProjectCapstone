03b92a4f66b1561650c4060f65e7df052a80ac6f
Pinda12 <vynnzo64@gmail.com>
2024-06-01 03:03:46 +0800

    tambahan view

--- app/src/main/java/com/example/capstoneproject/ui/admin/DetailAdminActivity.kt
+++ app/src/main/java/com/example/capstoneproject/ui/admin/DetailAdminActivity.kt
@@ -1,4 +1,4 @@
-package com.example.capstoneproject.ui.account.editprofile
+package com.example.capstoneproject.ui.admin
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.capstoneproject.R
 
-class EditProfileActivity : AppCompatActivity() {
+class DetailAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_profile)
+        setContentView(R.layout.activity_detail_admin)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
